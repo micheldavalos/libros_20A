43173b14c353479e2ee174115d256ef6641ae818
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-02-11 07:13:33 -0600

    se agregan libros a las lista y se muestran en la terminal

--- mainwindow.py
+++ mainwindow.py
@@ -3,6 +3,7 @@ from ui_mainwindow import Ui_MainWindow
 from PySide2.QtCore import Slot
 
 class MainWindow(QMainWindow):
+    libros = []
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -10,7 +11,26 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.agregar.clicked.connect(self.agregar)
+        self.ui.mostrar.clicked.connect(self.mostrar)
 
     @Slot()
     def agregar(self):
-        print('Hola')
+        titulo = self.ui.titulo.text()
+        autor = self.ui.autor.text()
+        year = self.ui.year.value()
+        editorial = self.ui.editorial.text()
+
+        # print(titulo, autor, year, editorial)
+        libro = {
+            'titulo': titulo,
+            'autor': autor,
+            'year': year,
+            'editorial': editorial
+        }
+        #print(libro)
+        self.libros.append(libro)
+
+    @Slot()
+    def mostrar(self):
+        for libro in self.libros:
+            print(libro)
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -25,50 +25,58 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
+        self.year = QSpinBox(self.centralwidget)
+        self.year.setObjectName(u"year")
+        self.year.setMinimum(1920)
+        self.year.setMaximum(2020)
+        self.year.setValue(2020)
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-
-        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 2)
+        self.gridLayout.addWidget(self.year, 2, 1, 1, 1)
 
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
 
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.agregar = QPushButton(self.centralwidget)
+        self.agregar.setObjectName(u"agregar")
 
-        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 2)
-
-        self.label_3 = QLabel(self.centralwidget)
-        self.label_3.setObjectName(u"label_3")
-
-        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
-
-        self.spinBox = QSpinBox(self.centralwidget)
-        self.spinBox.setObjectName(u"spinBox")
-
-        self.gridLayout.addWidget(self.spinBox, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.agregar, 4, 0, 1, 3)
 
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
 
         self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
 
-        self.lineEdit_3 = QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.autor = QLineEdit(self.centralwidget)
+        self.autor.setObjectName(u"autor")
 
-        self.gridLayout.addWidget(self.lineEdit_3, 3, 1, 1, 2)
+        self.gridLayout.addWidget(self.autor, 1, 1, 1, 2)
 
-        self.agregar = QPushButton(self.centralwidget)
-        self.agregar.setObjectName(u"agregar")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
 
-        self.gridLayout.addWidget(self.agregar, 4, 0, 1, 3)
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.editorial = QLineEdit(self.centralwidget)
+        self.editorial.setObjectName(u"editorial")
+
+        self.gridLayout.addWidget(self.editorial, 3, 1, 1, 2)
+
+        self.titulo = QLineEdit(self.centralwidget)
+        self.titulo.setObjectName(u"titulo")
+
+        self.gridLayout.addWidget(self.titulo, 0, 1, 1, 2)
+
+        self.mostrar = QPushButton(self.centralwidget)
+        self.mostrar.setObjectName(u"mostrar")
+
+        self.gridLayout.addWidget(self.mostrar, 5, 0, 1, 3)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -86,10 +94,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Libros", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"T\u00edtulo:", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Autor:", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"A\u00f1o:", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Editorial:", None))
         self.agregar.setText(QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Editorial:", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"T\u00edtulo:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"A\u00f1o:", None))
+        self.mostrar.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
     # retranslateUi
 
