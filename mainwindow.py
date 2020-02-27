d8df708e68122fe1e04755fe636cae6d47892132
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-02-26 18:06:25 -0600

    se muestran los libros en la tabla

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox
 from ui_mainwindow import Ui_MainWindow
 from PySide2.QtCore import Slot
 import json
@@ -16,6 +16,59 @@ class MainWindow(QMainWindow):
 
         self.ui.actionGuardar.triggered.connect(self.guardar)
         self.ui.actionAbrir.triggered.connect(self.abrir)
+
+        self.ui.mostrar_tabla.clicked.connect(self.mostrar_tabla)
+        self.ui.buscar.clicked.connect(self.buscar)
+
+    @Slot()
+    def buscar(self):
+        print('buscar')
+        texto = self.ui.lineEdit_buscar.text()
+        libros = []
+        for libro in self.libros:
+            if texto == libro['autor']:
+                libros.append(libro)
+
+        if len(libros) == 0:
+            QMessageBox.warning(self, "Libros", "No encontraron libros")
+        else:
+            self.libros_tabla(libros)
+
+
+    def libros_tabla(self, libros):
+        self.ui.tabla.clear()
+        self.ui.tabla.setColumnCount(4)
+        self.ui.tabla.setRowCount(len(libros))
+
+        labels = ["Título", "Autor", "Año", "Editorial"]
+        self.ui.tabla.setHorizontalHeaderLabels(labels)
+
+        row = 0
+        for libro in libros:
+            titulo = libro['titulo']
+            autor = libro['autor']
+            year = libro['year']
+            editorial = libro['editorial']
+
+            titulo_item = QTableWidgetItem(titulo)
+            autor_item = QTableWidgetItem(autor)
+            year_item = QTableWidgetItem(str(year))
+            editorial_item = QTableWidgetItem(editorial)
+
+            self.ui.tabla.setItem(row, 0, titulo_item)
+            self.ui.tabla.setItem(row, 1, autor_item)
+            self.ui.tabla.setItem(row, 2, year_item)
+            self.ui.tabla.setItem(row, 3, editorial_item)
+
+            row += 1
+
+
+
+
+    @Slot()
+    def mostrar_tabla(self):
+        print('mostrar')
+        self.libros_tabla(self.libros)
 
     @Slot()
     def abrir(self):
@@ -60,3 +113,8 @@ class MainWindow(QMainWindow):
     def mostrar(self):
         for libro in self.libros:
             print(libro)
+        for libro in self.libros:
+            self.ui.salida.insertPlainText("Título:" + libro['titulo'] + "\n")
+            self.ui.salida.insertPlainText("Autor:" + libro['autor'] + "\n")
+            self.ui.salida.insertPlainText("Año:" + str(libro['year']) + "\n")
+            self.ui.salida.insertPlainText("Editorial:" + libro['editorial'] + "\n")
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -3,33 +3,53 @@
 ################################################################################
 ## Form generated from reading UI file 'mainwindow.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.14.1
+## Created by: Qt User Interface Compiler version 5.14.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
     QRect, QSize, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QFont,
     QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
     QRadialGradient)
 from PySide2.QtWidgets import *
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(284, 377)
+        MainWindow.resize(609, 515)
         self.actionAbrir = QAction(MainWindow)
         self.actionAbrir.setObjectName(u"actionAbrir")
         self.actionGuardar = QAction(MainWindow)
         self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout = QGridLayout(self.tab)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.year = QSpinBox(self.centralwidget)
+        self.label = QLabel(self.tab)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.titulo = QLineEdit(self.tab)
+        self.titulo.setObjectName(u"titulo")
+
+        self.gridLayout.addWidget(self.titulo, 0, 1, 1, 1)
+
+        self.label_4 = QLabel(self.tab)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+
+        self.year = QSpinBox(self.tab)
         self.year.setObjectName(u"year")
         self.year.setMinimum(1920)
         self.year.setMaximum(2020)
@@ -37,55 +57,79 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.year, 2, 1, 1, 1)
 
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-
-        self.agregar = QPushButton(self.centralwidget)
-        self.agregar.setObjectName(u"agregar")
-
-        self.gridLayout.addWidget(self.agregar, 4, 0, 1, 3)
-
-        self.label_4 = QLabel(self.centralwidget)
-        self.label_4.setObjectName(u"label_4")
-
-        self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
-
-        self.autor = QLineEdit(self.centralwidget)
-        self.autor.setObjectName(u"autor")
-
-        self.gridLayout.addWidget(self.autor, 1, 1, 1, 2)
-
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-
-        self.label_3 = QLabel(self.centralwidget)
+        self.label_3 = QLabel(self.tab)
         self.label_3.setObjectName(u"label_3")
 
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
 
-        self.editorial = QLineEdit(self.centralwidget)
+        self.label_2 = QLabel(self.tab)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.autor = QLineEdit(self.tab)
+        self.autor.setObjectName(u"autor")
+
+        self.gridLayout.addWidget(self.autor, 1, 1, 1, 1)
+
+        self.editorial = QLineEdit(self.tab)
         self.editorial.setObjectName(u"editorial")
 
-        self.gridLayout.addWidget(self.editorial, 3, 1, 1, 2)
+        self.gridLayout.addWidget(self.editorial, 3, 1, 1, 1)
 
-        self.titulo = QLineEdit(self.centralwidget)
-        self.titulo.setObjectName(u"titulo")
+        self.agregar = QPushButton(self.tab)
+        self.agregar.setObjectName(u"agregar")
 
-        self.gridLayout.addWidget(self.titulo, 0, 1, 1, 2)
+        self.gridLayout.addWidget(self.agregar, 4, 0, 1, 2)
 
-        self.mostrar = QPushButton(self.centralwidget)
+        self.salida = QPlainTextEdit(self.tab)
+        self.salida.setObjectName(u"salida")
+
+        self.gridLayout.addWidget(self.salida, 0, 2, 6, 1)
+
+        self.mostrar = QPushButton(self.tab)
         self.mostrar.setObjectName(u"mostrar")
 
-        self.gridLayout.addWidget(self.mostrar, 5, 0, 1, 3)
+        self.gridLayout.addWidget(self.mostrar, 5, 0, 1, 2)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.verticalLayout = QVBoxLayout(self.tab_2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.lineEdit_buscar = QLineEdit(self.tab_2)
+        self.lineEdit_buscar.setObjectName(u"lineEdit_buscar")
+
+        self.horizontalLayout.addWidget(self.lineEdit_buscar)
+
+        self.buscar = QPushButton(self.tab_2)
+        self.buscar.setObjectName(u"buscar")
+
+        self.horizontalLayout.addWidget(self.buscar)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.tabla = QTableWidget(self.tab_2)
+        self.tabla.setObjectName(u"tabla")
+
+        self.verticalLayout.addWidget(self.tabla)
+
+        self.mostrar_tabla = QPushButton(self.tab_2)
+        self.mostrar_tabla.setObjectName(u"mostrar_tabla")
+
+        self.verticalLayout.addWidget(self.mostrar_tabla)
+
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.gridLayout_2.addWidget(self.tabWidget, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 284, 22))
+        self.menubar.setGeometry(QRect(0, 0, 609, 22))
         self.menuArchivo = QMenu(self.menubar)
         self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
@@ -98,6 +142,9 @@ class Ui_MainWindow(object):
         self.menuArchivo.addAction(self.actionGuardar)
 
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -112,12 +159,16 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.actionGuardar.setShortcut(QCoreApplication.translate("MainWindow", u"Meta+S", None))
 #endif // QT_CONFIG(shortcut)
+        self.label.setText(QCoreApplication.translate("MainWindow", u"T\u00edtulo:", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Editorial:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"A\u00f1o:", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Autor:", None))
         self.agregar.setText(QCoreApplication.translate("MainWindow", u"Agregar", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Editorial:", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"T\u00edtulo:", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"A\u00f1o:", None))
         self.mostrar.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.buscar.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+        self.mostrar_tabla.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Buscar", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
